421020d44cfa62106a7c94f806af33bac17ec397
PavelKorchevskiy <pavel.korchevskiy@mail.ru>
2020-12-09 11:46:52 +0300

    Add method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     private static final String HELLO = "Hello world";
     public static void main(String[] args) {
-        System.out.println(HELLO);
+        System.out.println(getHelloMessage());
 
+    }
+    public static String getHelloMessage() {
+        return HELLO + "! This is the best app";
     }
 }
