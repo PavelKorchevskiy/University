f4f00c457e172e927578b6a36afee61c5238677f
PavelKorchevskiy <pavel.korchevskiy@mail.ru>
2020-12-09 11:42:02 +0300

    Add variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
+    private static final String HELLO = "Hello world";
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hi");
+        System.out.println(HELLO);
+
     }
 }
