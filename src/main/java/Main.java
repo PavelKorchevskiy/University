62d84f62cfe945a5c32ae165a12aa526156f30d9
PavelKorchevskiy <pavel.korchevskiy@mail.ru>
2020-12-09 16:18:31 +0300

    Add tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ public class Main {
     private static final String HELLO = "Hello world";
     public static void main(String[] args) {
         System.out.println(getHelloMessage());
-
     }
     public static String getHelloMessage() {
         return HELLO + "! This is the best app";
