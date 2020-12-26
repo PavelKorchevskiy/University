36291940b81c743f0e7e247099a45d72881f4a8d
PavelKorchevskiy <pavel.korchevskiy@mail.ru>
2020-12-26 13:19:45 +0300

    Clean

--- controller/src/main/java/org/example/controller/ControllerForChangeRating.java
+++ controller/src/main/java/org/example/controller/ControllerForChangeRating.java
@@ -4,6 +4,8 @@ import org.example.model.Student;
 import org.example.model.Teacher;
 import org.example.repository.RepositoryForTeachersInMemory;
 import org.example.repository.RepositoryForTeachersInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,6 +18,7 @@ import java.util.Optional;
 
 @WebServlet("/changeRating")
 public class ControllerForChangeRating extends HttpServlet {
+    static final Logger log = LoggerFactory.getLogger(ControllerForChangeRating.class);
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -26,6 +29,7 @@ public class ControllerForChangeRating extends HttpServlet {
             //нужно ли кидать исключение в таком случае?
             //пусть пока ни чего не происходит
         }
+        log.info(req.getParameter("subjectStudent"));
         String subject = req.getParameter("subjectStudent");
         String login = req.getParameter("loginStudent");
         RepositoryForTeachersInterface repository = RepositoryForTeachersInMemory.getInstance();
